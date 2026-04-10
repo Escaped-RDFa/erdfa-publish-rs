253a3a612ccc48092978dd9c7bbed0b0796ee001
mike dupont <gemini-agent-47@solfunmeme.com>
2026-04-10 10:01:13 -0400

    chore: add rust-ipfs flake input for ipfs feature

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-ipfs = {
+      url = "github:meta-introspector/rust-ipfs";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-ipfs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -21,17 +25,24 @@
               lockFile = ./Cargo.lock;
               allowBuiltinFetchGit = true;
             };
-            buildFeatures = [ "native" "cli" ];
+            buildFeatures = [ "native" "cli" "ipfs" ];
             buildNoDefaultFeatures = true;
+
+            unpackPhase = ''
+              runHook preUnpack
+              cp -r --no-preserve=mode $src erdfa-publish
+              mkdir -p erdfa-publish/vendor
+              cp -r --no-preserve=mode ${rust-ipfs} erdfa-publish/vendor/rust-ipfs
+              sourceRoot=$PWD/erdfa-publish
+              runHook postUnpack
+            '';
           };
 
           default = self.packages.${system}.erdfa-publish;
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo rustc rust-analyzer rustfmt clippy
-          ];
+          buildInputs = with pkgs; [ cargo rustc rust-analyzer rustfmt clippy ];
         };
       }
     );
